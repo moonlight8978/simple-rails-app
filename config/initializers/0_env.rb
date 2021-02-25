4155e7a58bbfbb442075cbadaa374f05fa06dc0b
Le Si Bich <bich.le@impl.vn>
2021-02-25 10:58:10 +0700

    Fix SSM response always returns 1 record

--- config/initializers/0_env.rb
+++ config/initializers/0_env.rb
@@ -1,6 +1,6 @@
 if Feature.use_ssm?
   client = Aws::SSM::Client.new
-  result = client.get_parameters_by_path(path: ENV["SSM_PARAMETERS_PATH"], max_results: 1)
+  result = client.get_parameters_by_path(path: ENV["SSM_PARAMETERS_PATH"])
   result.parameters.each do |parameter|
     env_name = parameter.name.split("/").last
     env_value = parameter.value
