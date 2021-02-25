8416285aa6b135dd57e70bde6b24efeecdcea712
Le Si Bich <bich.le@impl.vn>
2021-02-25 15:23:54 +0700

    Move active storage inside environments

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-require_relative '../app/lib/feature'
 Dir[File.join(__dir__, 'preconfig', '*.rb')].each { |file| require file }
 
 # Require the gems listed in Gemfile, including any gems
@@ -15,7 +14,5 @@ module App
 
     config.time_zone = ActiveSupport::TimeZone::MAPPING["Hanoi"]
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.active_storage.service = Feature.use_s3? ? :s3 : :local
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require_relative "../../app/lib/feature"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -38,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :local
+  config.active_storage.service = Feature.use_s3? ? :s3 : :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
