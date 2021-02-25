63da3824a9ae8fa72e40f965812565633b6b0af1
Le Si Bich <bich.le@impl.vn>
2021-02-25 10:43:40 +0700

    Fix SSM

--- config/initializers/0_env.rb
+++ config/initializers/0_env.rb
@@ -1,7 +1,7 @@
 if Feature.use_ssm?
   client = Aws::SSM::Client.new
-  parameters = client.get_parameters_by_path(path: ENV["SSM_PARAMETERS_PATH"], max_results: 1)
-  parameters.each do |parameter|
+  result = client.get_parameters_by_path(path: ENV["SSM_PARAMETERS_PATH"], max_results: 1)
+  result.parameters.each do |parameter|
     env_name = parameter.name.split("/").last
     env_value = parameter.value
     ENV[env_name] = env_value
