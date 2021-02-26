bfc379b7fd9ea0a03a60db87cee44c59ce5aac6e
Le Si Bich <bich.le@impl.vn>
2021-02-26 15:43:24 +0700

    Remove test folder

--- test/controllers/passwords_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PasswordsControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get passwords_edit_url
-    assert_response :success
-  end
-end
--- test/controllers/profiles_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ProfilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get profiles_edit_url
-    assert_response :success
-  end
-end
